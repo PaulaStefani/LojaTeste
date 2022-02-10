b913a75f34183383d264e533a7b176c4e171aeac
PaulaStefani <96897101+PaulaStefani@users.noreply.github.com>
2022-02-09 21:05:04 -0300

    Update Fornecedor.kt

--- Fornecedor.kt
+++ Fornecedor.kt
@@ -1,7 +1,17 @@
-
 class Fornecedor {
     var nome: String? = null
+        get() = if (field.isNullOrEmpty()) "Nome do fornecedor não informado" else field
+        set(value) {
+            field = if (value!!.length < 2) null else value
+        }
     var email: String? = null
+        get() = if (field.isNullOrEmpty()) "Email do fornecedor não informado" else field
+        set(value) {
+            field = if (value.isNullOrEmpty()) null else value
+        }
     var telefoneComercial: String? = null
-
+        get() = if (field.isNullOrEmpty()) "Telefone comercial do fornecedor não infomado" else field
+        set(value) {
+            field = if (value!!.length >= 11) value else null
+        }
 }
