074bbd32d824155066223a96dd8fb94acba1f35d
PaulaStefani <96897101+PaulaStefani@users.noreply.github.com>
2022-02-10 15:01:03 -0300

    Update Filial.kt

--- Filial.kt
+++ Filial.kt
@@ -1,5 +1,16 @@
 class Filial {
-    var endereco: String? = null
-    var quantidadeFuncionario: String? = null
+    var email: String? = null
+        get() = if (field.isNullOrEmpty()) "Email da filial não foi informado" else field set (value){
+            field = if (value.isNullOrEmpty()) null else value
+        }
+    var quantidadeFuncionarios: String? = null
+        get() = if (field.isNullOrEmpty()) "Quantidade de funcionários da filial não foi informada" else field set (value){
+            field = if (value!!.length < 10) null else value
+        }
+
     var telefonecomercial: String? = null
+        get() = if (field.isNullOrEmpty()) "Telefone comercial da filial não foi informado" else field set (value){
+            field = if (field!!.length >= 11) null else value
+        }
+
 }
