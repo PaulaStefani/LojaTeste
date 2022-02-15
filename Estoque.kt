08db2c71e0e81833fe8d5552b6c1d4b9be926e14
PaulaStefani <96897101+PaulaStefani@users.noreply.github.com>
2022-02-14 22:51:57 -0300

    Update Estoque.kt

--- Estoque.kt
+++ Estoque.kt
@@ -1,13 +1,16 @@
 class estoque {
-    var quantidadeDisponivel: String? = null
-    get() = if (field.isNullOrEmpty()) "Quantidade disponível não informada" else field set (value){
-        field = if (value.isNullOrEmpty()) null else value
+    var quantidadeDisponivel: Float? = null
+   get() = if(field.isNullOrEmpty()) "Quantidade disponível não registrada" else field
+        set(value) {
+            if (value.isNullOrEmpty()) null else value
+        }
     }
     var produto: String? = null
-    get() = if (field.isNullOrEmpty()) "Produto não informado" else field set (value){
-        field = if (value.isNullOrEmpty()) null else value
-    }
-    var quantidadeVendida: String? = null
+      get() = if(field.isNullOrEmpty()) "Não há produtos registrados" else field
+        set(value) {
+            if (value.isNullOrEmpty()) null else value
+        }
+    var quantidadeVendida: Flaot? = null
     get() = if (field.isNullOrEmpty()) "Quantidade disponível não informada" else field set (value){
         field = if (value.isNullOrEmpty()) null else value
     }
